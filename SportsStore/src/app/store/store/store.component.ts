9dfa9484a27310ff48669a5b9c09fa60ff8dcdcb
Elsawaf001 <71516525+Elsawaf001@users.noreply.github.com>
2023-01-11 16:33:59 +0200

    2 files modified

--- SportsStore/src/app/store/store/store.component.ts
+++ SportsStore/src/app/store/store/store.component.ts
@@ -5,19 +5,47 @@ import { ProductRepository } from '../../../../../SportsStore/src/app/model/prod
 @Component({
   selector: 'app-store',
   templateUrl: './store.component.html',
-  styleUrls: ['./store.component.css']
+  styleUrls: ['./store.component.css'],
 })
 export class StoreComponent {
+  selectedCategory: string | undefined;
+  productsPerPage = 4;
+  selectedPage = 1;
 
-  constructor(private repository: ProductRepository){
+  constructor(private repository: ProductRepository) {}
 
+  get products(): Product[] {
+    let pageIndex = this.selectedPage - 1 + this.productsPerPage;
+    return this.repository
+      .getProducts(this.selectedCategory)
+      .slice(pageIndex, pageIndex + this.productsPerPage);
   }
 
-  get products():Product[]{
-    return this.repository.getProducts();
-  }
-
-  get categories(): string[]{
+  get categories(): string[] {
     return this.repository.getCategories();
+  }
+
+  changeCategory(newCategory?: string) {
+    this.selectedCategory = newCategory;
+  }
+
+  changePage(newPage: number) {
+    this.selectedPage = newPage;
+  }
+
+  changePageSize(newSize: number) {
+    this.productsPerPage = Number(newSize);
+    this.changePage(1);
+  }
+
+  get PageNumbers(): number[] {
+    return Array(
+      Math.ceil(
+        this.repository.getProducts(this.selectedCategory).length /
+          this.productsPerPage
+      )
+    )
+      .fill(0)
+      .map((x, i) => i + 1);
   }
 }
