565d8baac784a5bf73a5f825cc95c3e87f380077
Elsawaf001 <71516525+Elsawaf001@users.noreply.github.com>
2023-01-11 16:44:15 +0200

    3 files modified, 1 added

--- SportsStore/src/app/store/store.module.ts
+++ SportsStore/src/app/store/store.module.ts
@@ -3,9 +3,10 @@ import { ModelModule } from '../../../../SportsStore/src/app/model/model.module'
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { StoreComponent } from '../../../../SportsStore/src/app/store/store/store.component';
+import { CounterDirective } from '../../../../SportsStore/src/app/store/counter.directive';
 @NgModule({
   imports : [ModelModule , BrowserModule , FormsModule] ,
-  declarations  : [StoreComponent] ,
+  declarations  : [StoreComponent,CounterDirective] ,
   exports  : [StoreComponent]
 })
 export class StoreModule{
